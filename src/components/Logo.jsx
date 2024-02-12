f9efa8bda4cd9693a7412cc8d2f3f1ed91b3e73e
Gourav <mrgoravsainimrt@gmail.com>
2024-02-12 20:55:06 +0530

    first commit

---
+++ src/components/Logo.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Logo({width = '100px'}) {
+  return (
+    <div>
+      <img src="https://static.thenounproject.com/png/962492-200.png"
+      className='w-8 h-8' alt="" />
+    </div>
+  )
+}
+
+export default Logo
